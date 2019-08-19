5672cabdad304f081521c1b367d85c97bcbba21a
cancerwu <386899520@qq.com>
2019-08-19 22:35:19 +0800

    拦截器+login 8.19  22:35

--- src/main/java/org/lanqiao/jd/interceptor/WebConfigurer.java
+++ src/main/java/org/lanqiao/jd/interceptor/WebConfigurer.java
@@ -21,6 +21,7 @@ public class WebConfigurer implements WebMvcConfigurer {
         // excludePathPatterns("/login", "/register") 表示除了登陆与注册之外，因为登陆注册不需要登陆也可以访问
         InterceptorRegistration loginRegistry = registry.addInterceptor(loginInterceptor).addPathPatterns("/**");
         loginRegistry.excludePathPatterns("/login.html","/login","/index.html","/getLoginUserName","/mkorder.html","/selorder.html","selectAll/6","/getOrdersId","/selectOrdersId","/getAllU/{user_id}");
+        loginRegistry.excludePathPatterns("/JDregister.html","/insertUser","/checkSamePhone","/checkSameName");
         loginRegistry.excludePathPatterns("/css/*");
         loginRegistry.excludePathPatterns("/js/*");
         loginRegistry.excludePathPatterns("/images/*");
