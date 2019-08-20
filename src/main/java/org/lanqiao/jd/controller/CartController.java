cb15cb31946aff5448e6567cdfba50167d7aca87
zhangchengnuo1 <396545550@qq.com>
2019-08-20 11:59:28 +0800

    商品详情3 添加购物车3

--- src/main/java/org/lanqiao/jd/controller/CartController.java
+++ src/main/java/org/lanqiao/jd/controller/CartController.java
@@ -1,8 +1,10 @@
 package org.lanqiao.jd.controller;
 
+import org.lanqiao.jd.entity.Cart;
 import org.lanqiao.jd.entity.Cartdetail;
 import org.lanqiao.jd.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,4 +17,13 @@ public class CartController {
     public int insertSelective(Cartdetail record) {
         return cartService.insertSelective(record);
     }
+    @RequestMapping("/insertCar")
+    public int insertSelective(Cart cart) {
+        return cartService.insertSelective(cart);
+    }
+    @RequestMapping("/getcart")
+    public int selectByPrimaryKey(Integer cart_id) {
+        return cartService.selectByForKey(cart_id);
+    }
+
 }
--- src/main/java/org/lanqiao/jd/service/CartService.java
+++ src/main/java/org/lanqiao/jd/service/CartService.java
@@ -1,9 +1,12 @@
 package org.lanqiao.jd.service;
 
+import org.lanqiao.jd.entity.Cart;
 import org.lanqiao.jd.entity.Cartdetail;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CartService {
     int insertSelective(Cartdetail record);
+    int insertSelective(Cart cart);
+    int selectByForKey(Integer user_id);
 }
